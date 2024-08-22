41157e464a7e81cd384ae2363700e761c0b7a09a
life-random <80086175+life-random@users.noreply.github.com>
2024-08-22 17:47:56 +0900

    Update face_landmark_main.py

--- face_landmark_main.py
+++ face_landmark_main.py
@@ -1,39 +1,44 @@
-"""
-작성자 : 최병권
-작성일 : 2024 08 14
-최종 편집일 : 2024 08 14
-
-요약
-    텔로드론과 페이스트레킹 기술을 이용하여 추적하는 AI 드론의 메인 코드
-
-상세
-    face_landmark_class 파일의 FaceTracking 클래스가 있어야 얼굴인식이 가능함
-    드론 관련 조작키가 아직 추가 되지 않음
-    향후 클래스로 추가할 예정
-"""
-from djitellopy import tello
+from DlibFaceFlowingFace import face_detactor
+from DlibFaceFlowingDrone import drone
 import cv2
-from face_landmark_class import FaceTracking
+
+def main():
+    # face_recongnition 과 tello drone 객체 생성
+    face = face_detactor()
+    drone_control = drone()
+
+    # 드론 띄우기 및 드론 화면 활성화
+    drone_control.set_drone()
+    drone_control.take_off_drone()
+
+    # 추적할 얼굴 데이터화
+    face.encode_target_face()
+
+
+    try:
+        while True:
+
+            # 드론 화면 가져오기
+            img = drone_control.get_frame()
+
+            # 드론 화면 크기 조절
+            face.resize_frame(img)
+
+            # 인식한 얼굴 확인
+            face.detect_and_match_faces()
+            xVal, yVal, zVal = face.chack_face()
+            drone_control.tracking_face( xVal,  yVal,  zVal)
+            face.show_frame()
+
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                break
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    #드론 착륙 및 모든 화면 종류
+    drone_control.land_drone()
+    face.all_end()
 
 if __name__ == "__main__":
-    face_tracker = FaceTracking("./faces/choi.jpg")
-
-    me = tello.Tello()
-    me.connect()
-    print(me.get_battery())
-    me.streamoff()
-    me.streamon()
-
-    while True:
-        img = me.get_frame_read().frame
-        img = cv2.resize(img, (640, 480))
-
-        face_tracker.detect_and_match_faces(img)
-        imgStacked = face_tracker.track_face(img)
-
-        cv2.imshow("Image Stacked", imgStacked)
-
-        if cv2.waitKey(5) & 0xFF == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
+    main()
